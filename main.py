1cc67471730a98079e6d74cd6ec7cf0443b88d12
Micael Cosmo <micael.trabalho@hotmail.com>
2024-05-21 19:28:46 -0300

    feat: Adding loop to main
    O arquivo é capaz de orquestrar as diferentes funcionalidades gerando um resultado comum para a automação. Aguardando 5 segundos atualmente como intervalo de cada requisição para api do Telegram.

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 import logging
 from dotenv import load_dotenv
+from time import sleep
 
-# from data_treatment.treat_data import FileAnalyzer
-# from data_treatment.data_validate import JSONReader
+from data_treatment.treat_data import FileAnalyzer
+from data_treatment.data_validate import JSONReader
 from get_data import get_data_from_telegram as gd_telegram
 
 
@@ -19,8 +20,31 @@ class Main:
         pass
 
     def run(self):
-        # First, get the data from Telegram
-        gd_telegram.loop_main()
+        while True:
+            # First, get the data from Telegram
+            gd_telegram.loop_main()
+            # Then, validate the data
+            file_analyzer = FileAnalyzer('group_messages.txt')
+            json_reader = JSONReader('rescource/data.json')
+            new_data = file_analyzer.find_pattern()
+            if json_reader.read_json() == True and new_data != None:
+                logging.info("Data read successfully.")
+                json_reader.get_date_from_data(new_data)
+                different_dates = json_reader.different_dates(date=json_reader.current_date)
+                if different_dates:
+                    # Finally, treat and save the new data
+                    logging.info("Dates are different. Updating date and saving data.")
+                    file_analyzer.save_to_json(data=new_data, file_path='rescource/data.json')
+                else:
+                    logging.info("Nothing to do. Waiting for the next iteration.")
+            else:
+                # Finally, treat and save the new data
+                new_data = file_analyzer.find_pattern()
+                if new_data != None:
+                    logging.info("Data saved successfully.")
+                    file_analyzer.save_to_json(data=new_data, filename='rescource/data.json')
+            sleep(5)
+
 
 
 
