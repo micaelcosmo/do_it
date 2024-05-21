cae323aa8f36cc75171cc78991b84df18cd5887c
Micael Cosmo <micael.trabalho@hotmail.com>
2024-05-21 13:59:32 -0300

    refactor: Translated to English
    Manter o padrão. =)

--- data_treatment/treat_data.py
+++ data_treatment/treat_data.py
@@ -23,52 +23,52 @@ pattern_link = os.getenv('PATTERN_LINK')
 pattern_data_hora = os.getenv('PATTERN_DATA_HORA')
 
 
-class AnalisadorArquivo:
-    def __init__(self, nome_arquivo):
-        self.nome_arquivo = nome_arquivo
+class FileAnalyzer:
+    def __init__(self, file_path):
+        self.file_path = file_path
         
 
-    def encontrar_padrao(self):
+    def find_pattern(self):
         """
         Find and extract patterns from the file.
 
         Returns:
             dict: A dictionary containing the extracted data.
         """
-        with open(self.nome_arquivo, 'r', encoding='utf-8') as arquivo:
-            linhas = arquivo.readlines()
-            padrao_inicio = fr"{target_phone_number}{pattern_start}"
-            indice_inicio = linhas.index(padrao_inicio + "\n") + 1
-            quantidade_linhas = 13  # Defina a quantidade de linhas que você deseja após o início do padrão
-            dados_tratados = {}
-            if indice_inicio != -1:
+        with open(self.file_path, 'r', encoding='utf-8') as arquivo:
+            lines = arquivo.readlines()
+            pattern_start_string = fr"{target_phone_number}{pattern_start}"
+            start_index = lines.index(pattern_start_string + "\n") + 1
+            line_count = 13  # Defina a quantidade de lines que você deseja após o início do padrão
+            treated_data = {}
+            if start_index != -1:
                 logging.info("PADRÃO ENCONTRADO - INÍCIO")
-                for linha in linhas[indice_inicio:indice_inicio+quantidade_linhas]:
+                for linha in lines[start_index:start_index+line_count]:
                     if re.match(fr"{pattern_roleta}", linha):
-                        dados_tratados['roleta'] = re.search(fr"{pattern_roleta}", linha).group(1)
+                        treated_data['roleta'] = re.search(fr"{pattern_roleta}", linha).group(1)
                         logging.debug("Roleta: %s", re.search(fr"{pattern_roleta}", linha).group(1))
                     elif re.match(fr"{pattern_estrategia}", linha):
-                        dados_tratados['estrategia'] = re.search(fr"{pattern_estrategia}", linha).group(1)
+                        treated_data['estrategia'] = re.search(fr"{pattern_estrategia}", linha).group(1)
                         logging.debug("Estratégia: %s", re.search(fr"{pattern_estrategia}", linha).group(1))
                     elif re.match(fr"{pattern_apostar}", linha):
-                        dados_tratados['apostar'] = re.search(fr"{pattern_apostar}", linha).group(1)
+                        treated_data['apostar'] = re.search(fr"{pattern_apostar}", linha).group(1)
                         logging.debug("Apostar: %s", re.search(fr"{pattern_apostar}", linha).group(1))
                     elif re.match(fr"{pattern_numeros}", linha):
                         numeros = re.search(fr"{pattern_numeros}", linha).group(1).strip().replace("**", "").split(" | ")
-                        dados_tratados['numeros'] = [int(numero) for numero in numeros]
+                        treated_data['numeros'] = [int(numero) for numero in numeros]
                         logging.debug("Números: %s", numeros)
                     elif re.match(fr"{pattern_cobrir_zero}", linha):
-                        dados_tratados['cobrir_zero'] = re.search(fr"{pattern_cobrir_zero}", linha).group(1).replace("**", "")
+                        treated_data['cobrir_zero'] = re.search(fr"{pattern_cobrir_zero}", linha).group(1).replace("**", "")
                         logging.debug("Cobrir Zero: %s", re.search(fr"{pattern_cobrir_zero}", linha).group(1).replace("**", ""))
                     elif re.match(fr"{pattern_link}", linha):
-                        dados_tratados['link'] = re.search(fr"{pattern_link}", linha).group(1)
+                        treated_data['link'] = re.search(fr"{pattern_link}", linha).group(1)
                         logging.debug("Link: %s", re.search(fr"{pattern_link}", linha).group(1))
                     elif re.match(fr"{pattern_data_hora}", linha):
-                        dados_tratados['data_hora'] = re.match(fr"{pattern_data_hora}", linha).group().format("YYYY-MM-DD HH:mm:ss")
+                        treated_data['data_hora'] = re.match(fr"{pattern_data_hora}", linha).group().format("YYYY-MM-DD HH:mm:ss")
                         logging.debug("Data e Hora: %s", re.match(fr"{pattern_data_hora}", linha).group())
-                logging.info(json.dumps(dados_tratados, indent=4, ensure_ascii=False))
+                logging.info(json.dumps(treated_data, indent=4, ensure_ascii=False))
                 logging.info("PADRÃO ENCONTRADO - FIM")
-                return dados_tratados
+                return treated_data
             else:
                 logging.info("Padrão não encontrado.")
                 return None
@@ -90,6 +90,6 @@ class AnalisadorArquivo:
 
 
 if __name__ == "__main__":
-    analise = AnalisadorArquivo("group_messages.txt")
-    data = analise.encontrar_padrao()
+    analise = FileAnalyzer("group_messages.txt")
+    data = analise.find_pattern()
     analise.save_to_json(data, "data.json")
